8684bdeaf58dc3dfba374a88ac89a43fabb5c204
Patrick Ellis <oneredoak@gmail.com>
2014-07-15 13:38:43 -0700

    Copy of sample_node_selenium App

--- conf.js
+++ conf.js
@@ -1,4 +1,7 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js']
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  specs: ['spec.js'],
+  capabilities: {
+    browserName: "firefox"
+  },
 }
